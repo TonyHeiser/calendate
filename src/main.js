e82eefbb51a6177f71042aac9aa453d2806b5dd3
Tony Heiser <tonyhzer@gmail.com>
2026-06-27 16:45:15 +0500

    fix: calendar.js as module

--- src/main.js
+++ src/main.js
@@ -1,7 +1,2 @@
-import './style.css'
-
-document.querySelector('#app').innerHTML = `
-<h1>test</h1>
-`
-
-setupCounter(document.querySelector('#counter'))
+import './style.css';
+import "./calendar.js"
